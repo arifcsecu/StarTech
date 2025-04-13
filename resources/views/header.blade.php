d1de29dffd824b672e70375697a0df4e58c679f2
Ariful Islam <arifcsecu16@gmail.com>
2025-04-13 16:37:57 +0600

    header added

---
+++ resources/views/header.blade.php
@@ -0,0 +1,31 @@
+<header class="main-header py-3">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-3 col-md-4 col-6">
+                <a href="/">
+                    <img src="https://www.startech.com.bd/image/catalog/logo.png" alt="StarTech Logo"
+                        class="img-fluid">
+                </a>
+            </div>
+
+            <div class="col-lg-6 col-md-8 d-none d-md-block">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search">
+                    <button class="btn btn-primary" type="button"><i class="fas fa-search"></i></button>
+                </div>
+            </div>
+
+            <div class="col-lg-3 d-none d-lg-block text-end">
+                <div class="d-flex align-items-center justify-content-end">
+                    <div class="text-start me-3">
+                        <div class="fw-bold">Customer Care</div>
+                        <div class="small">09678 111 222</div>
+                    </div>
+                    <div>
+                        <i class="fas fa-headset fs-3 text-primary"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
